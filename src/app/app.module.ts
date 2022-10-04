7fe9d76ed017f1dbe2b65362f0b20ca00acb071e
Michael Xi <111757720+xingruoxicic@users.noreply.github.com>
2022-10-04 13:23:42 -0700

    Import ds angular components into app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { IRCCModule } from '@ircc-ca/ds-sdc-angular';
+import { JLButtonModule } from '@ircc-ca/ds-sdc-angular/button';
+import { JLCheckboxModule } from '@ircc-ca/ds-sdc-angular/checkbox';
+import { JLFooterModule } from '@ircc-ca/ds-sdc-angular/footer';
+import { JLHeaderModule } from '@ircc-ca/ds-sdc-angular/header';
+import { JLIconModule } from '@ircc-ca/ds-sdc-angular/icon';
+import { JLLinkModule } from '@ircc-ca/ds-sdc-angular/link';
+import { JLRadiobuttonModule } from '@ircc-ca/ds-sdc-angular/radio-button';
 
 import { AppComponent } from './app.component';
+
+const JL_ANGULAR_COMPONENTS = [
+  JLButtonModule,
+  JLIconModule,
+  JLLinkModule,
+  JLRadiobuttonModule,
+  JLCheckboxModule,
+  JLFooterModule,
+  JLHeaderModule,
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    IRCCModule,
+    JL_ANGULAR_COMPONENTS
   ],
   providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

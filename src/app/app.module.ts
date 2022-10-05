b30d3c78942eafeee13f1c59dafeb7696d7c77ad
Michael Xi <111757720+xingruoxicic@users.noreply.github.com>
2022-10-05 14:14:07 -0700

    Install ngx-markdown plugin, initialize with a demo md file.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule } from "@angular/common/http";
 import { IRCCModule } from '@ircc-ca/ds-sdc-angular';
 import { JLButtonModule } from '@ircc-ca/ds-sdc-angular/button';
 import { JLCheckboxModule } from '@ircc-ca/ds-sdc-angular/checkbox';
@@ -8,6 +9,7 @@ import { JLHeaderModule } from '@ircc-ca/ds-sdc-angular/header';
 import { JLIconModule } from '@ircc-ca/ds-sdc-angular/icon';
 import { JLLinkModule } from '@ircc-ca/ds-sdc-angular/link';
 import { JLRadiobuttonModule } from '@ircc-ca/ds-sdc-angular/radio-button';
+import { MarkdownModule } from "ngx-markdown";
 
 import { AppComponent } from './app.component';
 
@@ -28,10 +30,15 @@ const JL_ANGULAR_COMPONENTS = [
   imports: [
     BrowserModule,
     IRCCModule,
-    JL_ANGULAR_COMPONENTS
+    JL_ANGULAR_COMPONENTS,
+    HttpClientModule,
+    MarkdownModule.forRoot({ loader: HttpClient }),
   ],
   providers: [],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// enable default sanitization
+MarkdownModule.forRoot()

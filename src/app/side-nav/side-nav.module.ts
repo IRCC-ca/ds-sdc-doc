ce879e04873bb37e0cd625e5b12f3837ff196d62
Michael Xi <111757720+xingruoxicic@users.noreply.github.com>
2022-10-26 08:28:55 -0700

    Moved side-nav-container to folder

--- src/app/side-nav/side-nav.module.ts
+++ src/app/side-nav/side-nav.module.ts
@@ -1,6 +1,6 @@
 import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {SideNavContainerComponent} from "./side-nav-container.component";
+import {SideNavContainerComponent} from "./side-nav-container/side-nav-container.component";
 import {SideNavComponent} from "./side-nav.component";
 import { IRCCModule } from "@ircc-ca/ds-sdc-angular";
 

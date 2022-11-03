a659361d0a569e4dfbd38342c35978d8f016e612
Xingruo (Michael) Xi <111757720+xingruoxicic@users.noreply.github.com>
2022-11-03 13:56:40 -0700

    Install eslint for linting (#16)
    * Install eslint to Angular
    
    * Fix existing lint errors

--- src/app/pages/overview/overview.component.ts
+++ src/app/pages/overview/overview.component.ts
@@ -1,22 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import {ISideNavDataInterface} from "../../side-nav/side-nav.model";
-import {TranslateService} from "@ngx-translate/core";
-import { SideNavConfig} from "../../side-nav/side-nav.config";
-import { NavigationEnd, Router } from '@angular/router';
+import { Component } from '@angular/core';
+import { ISideNavDataInterface } from "../../side-nav/side-nav.model";
+import { TranslateService} from "@ngx-translate/core";
+import { SideNavConfig } from "../../side-nav/side-nav.config";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.scss']
 })
-export class OverviewComponent implements OnInit {
+export class OverviewComponent {
   rightNavData: ISideNavDataInterface[];
 
   constructor(private translate: TranslateService, private router: Router, private navBarConfig : SideNavConfig) {
     this.rightNavData = navBarConfig.getRightNavBarConfig(this.translate);
-  }
-
-  ngOnInit(): void {
   }
 
 }

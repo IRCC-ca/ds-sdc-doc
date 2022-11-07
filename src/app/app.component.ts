73c339c7c0c6adcb240fc3c63a2b8e9bdda3383b
Muhammad Naseer Ahmad <114417946+Naseer-007@users.noreply.github.com>
2022-11-07 16:50:55 -0500

    Added DJL header and footer and refactored logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
 import { SideNavConfig } from './side-nav/side-nav.config';
 import { ISideNavDataInterface } from './side-nav/side-nav.model';
-import {LocalizeRouterService} from "@gilsdav/ngx-translate-router";
+import {LocalizeNgModuleFactory, LocalizeRouterService} from "@gilsdav/ngx-translate-router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ds-sdc-doc';
   leftNavData: ISideNavDataInterface[];
+  language = this.getCurrentLang();
+  mobile = false;
+  public innerWidth: any;
 
   constructor(
     private translate: TranslateService,
@@ -22,11 +25,50 @@ export class AppComponent {
     this.leftNavData = navBarConfig.getLeftNavBarConfig(this.translate);
   }
 
-  useLanguage(language: string): void {
+  ngOnInit() {
+    this.onResize()
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.innerWidth = window.innerWidth;
+    if (this.innerWidth <= 992 ) {
+      this.mobile = true;
+      this.displayLanguage(this.getCurrentLang())
+    }
+    else {
+      this.mobile = false
+      this.displayLanguage(this.getCurrentLang())
+    }
+  }
+
+  displayLanguage(language : string) : string {
+    if (this.mobile) {
+      if (language === 'en') {
+        return this.language = 'FR';
+      } else {
+        return this.language = 'EN'
+      }
+    }
+    else {
+      if (language === 'en') {
+        return this.language = 'Français';
+      } else {
+        return this.language = 'English'
+      }
+    }
+  }
+
+  useLanguage(): void {
+    let language = this.getCurrentLang()
+    language === 'en' || language === 'English' ? language = "fr" : language = 'en'
     this.localize.changeLanguage(language);
+    
   }
 
   getCurrentLang(): string {
-    return this.translate.currentLang;
+    let currentLanguage = this.translate.currentLang;
+    this.displayLanguage(currentLanguage)
+    return currentLanguage;
   }
 }

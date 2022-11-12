fdbc01c94976ca59f81fdfa97518bb1608646631
Michael Xi <111757720+xingruoxicic@users.noreply.github.com>
2022-11-12 13:08:31 -0800

    title-slug-url: add translation binding from directive

--- src/app/title-slug-url/title-slug-url.component.ts
+++ src/app/title-slug-url/title-slug-url.component.ts
@@ -1,5 +1,6 @@
 import {AfterContentInit, Component, ElementRef, Input} from '@angular/core';
 import { SlugifyPipe } from "../share/pipe-slugify.pipe";
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
   selector: 'app-title-slug-url',
@@ -9,15 +10,23 @@ import { SlugifyPipe } from "../share/pipe-slugify.pipe";
 })
 export class TitleSlugUrlComponent implements AfterContentInit {
   @Input() type = 'secondary'; // primary | secondary
+  @Input() translate = ''; // translation binding
   title = '';
   titleSlugified = ''; // overview-test
   slugifiedUrl = ''; // http://localhost:4200/en/overview#overview-test
 
-  constructor(private el: ElementRef, private slugify: SlugifyPipe) {
+  constructor(
+    private el: ElementRef,
+    private slugify: SlugifyPipe,
+    private translator : TranslateService
+  ) {
   }
 
   ngAfterContentInit(): void {
-    this.title = this.el.nativeElement.childNodes[0].childNodes[0].data;
+    this.title = this.el.nativeElement.innerText;
+    if (this.translate != '') {
+      this.title = this.translator.instant(this.translate);
+    }
     this.titleSlugified = this.slugify.transform(this.title);
     this.slugifiedUrl = this.el.nativeElement.ownerDocument.location.origin
       + this.el.nativeElement.ownerDocument.location.pathname

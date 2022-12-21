cd03fa17653bc357c4a1351e32d3b7c2bedcc7dd
Muhammad Naseer Ahmad <114417946+Naseer-007@users.noreply.github.com>
2022-12-21 16:11:03 -0500

    refactored code and added logic for edge cases

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,10 @@ import {TranslateService} from '@ngx-translate/core';
 import { SideNavConfig } from './side-nav/side-nav.config';
 import { ISideNavDataInterface } from './side-nav/side-nav.model';
 import {LocalizeNgModuleFactory, LocalizeRouterService} from "@gilsdav/ngx-translate-router";
-import { DisplayLanguages, Languages } from './share/global-params';
+import { DefaultHomePage, DisplayLanguages, Languages, StorageKeys } from './share/global-params';
+import { Url } from './share/services/url.service';
+import { ComponentFactoryResolver } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -16,14 +19,16 @@ export class AppComponent implements OnInit {
   language = this.getCurrentLang();
   mobile = false;
   navStatus = 'nav-oepn';
+  locale: any = sessionStorage.getItem(StorageKeys.Locale)
   public innerWidth: any; // Width of viewport window
 
   constructor(
     private translate: TranslateService,
     private localize: LocalizeRouterService,
-    private navBarConfig : SideNavConfig
+    private navBarConfig : SideNavConfig,
   ) {
-    translate.setDefaultLang(Languages.English);
+    // translate.setDefaultLang(Languages.English);
+    this.setLanguage()
     this.leftNavData = navBarConfig.getLeftNavBarConfig(this.translate);
   }
 
@@ -46,26 +51,56 @@ export class AppComponent implements OnInit {
     }
   }
 
-  toggleLanguage(language : string) : void {
+  toggleLanguage(locale : string) : void {
     if (this.mobile) {
-      language === Languages.English ? this.language = DisplayLanguages.FR : this.language = DisplayLanguages.EN;
+      locale === Languages.English ? this.language = DisplayLanguages.FR : this.language = DisplayLanguages.EN;
     }
     else {
-      language === Languages.English ? this.language = DisplayLanguages.French : this.language = DisplayLanguages.English;
+      locale === Languages.English ? this.language = DisplayLanguages.French : this.language = DisplayLanguages.English;
+    }
+  }
+
+  setLanguage() {
+    let url = window.location.pathname // gett current url
+    let urlKeys = url.split("/")
+
+    // remove the slash at the first character of the url, which makes an empty element on the
+    // keys array, and add it back when return the new url
+    if (url[0] === '/') {
+      urlKeys.shift()
+    }
+    
+    let extractedLang = urlKeys[0]
+    const langs = ['en', 'fr']
+    if (!extractedLang || !langs.includes(extractedLang)){ //if invalid url navigate to default overview page
+      console.log("URL UNDEFINED")
+      sessionStorage.setItem(StorageKeys.Locale, Languages.English) // set default session storage to english
+      const locale: any = sessionStorage.getItem(StorageKeys.Locale)
+      this.translate.addLangs(langs)
+      this.translate.setDefaultLang(locale)
+      window.location.replace(Url.translateUrlWithLang(DefaultHomePage, locale))
+      return this.translate.use(locale)
+    }
+    else {
+      sessionStorage.setItem(StorageKeys.Locale, extractedLang)
+      const locale: any = sessionStorage.getItem(StorageKeys.Locale)
+      this.translate.addLangs(langs)
+      this.translate.setDefaultLang(locale)
+      return this.translate.use(locale)
     }
   }
 
   useLanguage(): void {
+    let currentUrl = window.location.pathname // get current url
     let currentLanguage = this.getCurrentLang();
-    let updatedLanguage = ""
-    currentLanguage === Languages.English || currentLanguage === DisplayLanguages.English ? updatedLanguage = Languages.French : updatedLanguage = Languages.English;
-    let pathName = window.location.pathname;
-    pathName = pathName.replace(currentLanguage, updatedLanguage);
-    window.location.replace(pathName) // navigate to path with updated language
+    let updatedLanguage = '';
+    currentLanguage === Languages.English ? updatedLanguage = Languages.French : updatedLanguage = Languages.English;
+    sessionStorage.setItem(StorageKeys.Locale, updatedLanguage)
+    window.location.replace(Url.translateUrlWithLang(currentUrl, updatedLanguage))
   }
 
   getCurrentLang(): string {
-    let currentLanguage = this.translate.currentLang;
+    let currentLanguage : any = sessionStorage.getItem(StorageKeys.Locale);
     this.toggleLanguage(currentLanguage);
     return currentLanguage;
   }
